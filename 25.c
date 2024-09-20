92f503a62a64cbfcdac5d8344049e51fcb09a288
Sakina27 <sakinabaranwala123@gmail.com>
2024-09-21 00:50:47 +0530

    25th Question

--- 25.c
+++ 25.c
@@ -1,18 +1,18 @@
 /*
-============================================================================
+================================================================================================
 Name : 25.c
 Author : Sakina Baranwala
 Description : Write a program to print a message queue's (use msqid_ds and ipc_perm structures)
-a. access permission
-b. uid, gid
-c. time of last message sent and received
-d. time of last change in the message queue
-d. size of the queue
-f. number of messages in the queue
-g. maximum number of bytes allowed
-h. pid of the msgsnd and msgrcv
+              a. access permission
+              b. uid, gid
+              c. time of last message sent and received
+              d. time of last change in the message queue
+              e. size of the queue
+              f. number of messages in the queue
+              g. maximum number of bytes allowed
+              h. pid of the msgsnd and msgrcv
 Date: 20th September, 2024
-============================================================================
+===============================================================================================
 */
 
 
