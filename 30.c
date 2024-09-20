c5927aeddce62ac7156def887e2e56feceb49d09
Sakina27 <sakinabaranwala123@gmail.com>
2024-09-21 00:52:24 +0530

    30th Question

--- 30.c
+++ 30.c
@@ -3,10 +3,10 @@
 Name : 30.c
 Author : Sakina Baranwala
 Description : Write a program to create a shared memory.
-a. write some data to the shared memory
-b. attach with O_RDONLY and check whether you are able to overwrite.
-c. detach the shared memory
-d. remove the shared memory
+              a. write some data to the shared memory
+              b. attach with O_RDONLY and check whether you are able to overwrite.
+              c. detach the shared memory
+              d. remove the shared memory
 Date: 20th September, 2024
 ============================================================================
 */
